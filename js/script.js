8bb6989f7715fef3fb0debd9a925270a1128e7f8
1849695 <162330085+1849695@users.noreply.github.com>
2024-07-02 16:13:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ import { TeapotBufferGeometry } from './threejs/teapotGeometry.js';
 let tabs = Array.prototype.slice.call(document.getElementsByTagName('li'));
 let pages = Array.prototype.slice.call(document.getElementsByClassName('page'));
 for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', tabClick(tabs, pages, i), false);
+    tabs[i].addEventListener('click', tabC(tabs, pages, i), false);
 }
-function tabClick(t, p, index) {
+function tabC(t, p, index) {
     return () => {
         for (let i = 0; i < t.length; i++) {
             t[i].className = '';
@@ -30,10 +30,10 @@ let rotation = 1;
 let isExploring = false;
 
 // canvas is 75% of full screen
-const scaleWindow = 0.75; 
+const scaleWindow = 0.80; 
 
 // images for the scene background
-const sceneImages = [
+const sceneIm = [
     "./img/px.png", 
     "./img/nx.png",
     "./img/py.png", 
@@ -54,22 +54,22 @@ renderer.gammaInput = true;
 renderer.gammaOutput = true;
 canvas.appendChild(renderer.domElement);
 
-// camera facing a side view of the teapot
+// camera setup with perspective view
 let camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 10000);
 camera.position.set(0, 500, 3000);
 
 // load background images into cubemap, apply lighting
-let background = new THREE.CubeTextureLoader().load(sceneImages);
+let background = new THREE.CubeTextureLoader().load(sceneIm);
 let teapotColor = new THREE.Color().setRGB(1.0, 1.0, 1.0);
-let reflectionTexture = new THREE.MeshPhongMaterial({ color: teapotColor, envMap: background, side: THREE.DoubleSide });
+let reflectionTxtr = new THREE.MeshPhongMaterial({ color: teapotColor, envMap: background, side: THREE.DoubleSide });
 let ambientLight = new THREE.AmbientLight(0x333333);
 let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-let teapotGeometry = new TeapotBufferGeometry(400, 15, true, true, true, false, true);
-let teapot = new THREE.Mesh(teapotGeometry, reflectionTexture);
+let teapotGeom = new TeapotBufferGeometry(400, 15, true, true, true, false, true);
+let teapot = new THREE.Mesh(teapotGeom, reflectionTxtr);
 
 // create the label above the teapot
-const labelCanvas = makeLabel();
-const texture = new THREE.CanvasTexture(labelCanvas);
+const labelCanv = makeLabel();
+const texture = new THREE.CanvasTexture(labelCanv);
 texture.minFilter = THREE.LinearFilter;
 texture.wrapS = THREE.ClampToEdgeWrapping;
 texture.wrapT = THREE.ClampToEdgeWrapping;
@@ -79,8 +79,8 @@ const labelMaterial = new THREE.SpriteMaterial({
 });
 const label = new THREE.Object3D();
 const sprite = new THREE.Sprite(labelMaterial);
-sprite.scale.x = labelCanvas.width;
-sprite.scale.y = labelCanvas.height;
+sprite.scale.x = labelCanv.width;
+sprite.scale.y = labelCanv.height;
 label.add(sprite);
 label.position.x = 0;
 label.position.y = 600;
@@ -124,39 +124,40 @@ document.getElementById('explore').addEventListener('click', () => {
     }
 }, false);
 
-// an animation loop for the teapot scene
-function render() {
-    requestAnimationFrame( render );
-    if(isExploring === false){
-        rotation += 0.001;
-        camera.position.x = Math.sin(rotation) * 2000;
-        camera.position.z = Math.cos(rotation) * 2000;
-        camera.lookAt( scene.position );
-    }
-    renderer.render( scene, camera );
-}
 
 function makeLabel() {
     const width = 1240;
     const height = 240;
 
-    const labelCanvas = document.createElement('canvas').getContext('2d');
-    labelCanvas.font = '200px bold sans-serif';
-    const textWidth = labelCanvas.measureText('The Utah Teapot').width;
+    const labelCanv = document.createElement('canvas').getContext('2d');
+    labelCanv.font = '200px bold sans-serif';
+    const textWidth = labelCanv.measureText('Teapot').width;
 
-    labelCanvas.canvas.width = width;
-    labelCanvas.canvas.height = height;
+    labelCanv.canvas.width = width;
+    labelCanv.canvas.height = height;
 
-    labelCanvas.font = '200px bold sans-serif';
-    labelCanvas.textBaseline = 'middle';
-    labelCanvas.textAlign = 'center';
-    labelCanvas.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    labelCanv.font = '200px bold sans-serif';
+    labelCanv.textBaseline = 'middle';
+    labelCanv.textAlign = 'center';
+    labelCanv.fillStyle = 'rgba(0, 0, 0, 0.25)';
 
-    labelCanvas.fillRect(0, 0, width, height);
-    labelCanvas.translate(width / 2, height / 2);
-    labelCanvas.scale(1200 / textWidth, 1);
-    labelCanvas.fillStyle = 'white';
-    labelCanvas.fillText('The Utah Teapot', 0, 0);
+    labelCanv.fillRect(0, 0, width, height);
+    labelCanv.translate(width / 2, height / 2);
+    labelCanv.scale(1200 / textWidth, 1);
+    labelCanv.fillStyle = 'black';
+    labelCanv.fillText('The Utah Teapot', 0, 0);
 
-    return labelCanvas.canvas;
+    return labelCanv.canvas;
   }
+
+  // an animation loop for the teapot scene
+function render() {
+    requestAnimationFrame( render );
+    if(isExploring === false){
+        rotation += 0.001;
+        camera.position.x = Math.sin(rotation) * 3000;
+        camera.position.z = Math.cos(rotation) * 3000;
+        camera.lookAt( scene.position );
+    }
+    renderer.render( scene, camera );
+}
